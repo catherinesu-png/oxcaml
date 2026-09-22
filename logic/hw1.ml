813962c22aa3a910057a143a2afcd6935756a6e4
Catherine Su <155842645+catherinesu-png@users.noreply.github.com>
2026-09-22 18:06:08 +0000

    homework 1

--- logic/hw1.ml
+++ logic/hw1.ml
@@ -1,104 +1,96 @@
-open! Core
+type arrow_direction = 
+     | Left
+     | Down
+     | Up 
+     | Right 
 
-type player_kind =
-  | X
-  | O
-
-type cell_position =
-  { row : int
-  ; column : int
+type note = 
+  { timestamp : float 
+  ; direction : arrow_direction
   }
 
-type decision =
-  | In_progress of { whose_turn : player_kind }
-  | Winner of player_kind
-  | Stalemate
+type judgement = 
+     | Marvelous
+     | Perfect
+     | Great
+     | Good
+     | Miss
+
+type decision = 
+     | In_progress
+     | Stage_cleared of { final_score : int }
+     | Stage_failed
 
 type game_state =
-  { board : (cell_position * player_kind) list
-  ; rows : int
-  ; columns : int
-  ; winning_sequence_length : int
+  { upcoming_notes : note list
+  ; life_bar : float
+  ; score : int 
+  ; combo : int 
+  ; song_elapsed : float 
   ; decision : decision
   }
 
-type move = cell_position
+type move = 
+  { key_pressed : arrow_direction 
+  ; press_time : float
+  }
 
-(*=
- | |
------
- | |
------
- | |
-*)
-let initial_state : game_state =
-  { board = []
-  ; rows = 3
-  ; columns = 3
-  ; winning_sequence_length = 3
-  ; decision = In_progress { whose_turn = X }
+let evaluate_judgement (delta_time : float) judgment = 
+   let abs_delta = Float.abs delta_time in 
+   if abs_delta <= 0.02 then Marvelous
+   else if abs_delta <= 0.05 then Perfect 
+   else if abs_delta <= 0.09 then Great 
+   else if abs_delta <= 0.12 then Good
+   else Miss
+;; 
+
+let initial_state : game_state = 
+  { upcoming_notes = 
+       [ { timestamp = 1.0; direction = Left}
+       ; { timestamp = 1.5; direction = Up}
+       ; { timestamp = 2.0; direction = Down}]
+  ; life_bar = 100.0
+  ; score = 0
+  ; combo = 0 
+  ; song_elapsed = 0.0 
+  ; decision = In_progress
   }
 ;;
 
-let move_at_0x0 : move = { row = 0; column = 0 }
+let move_at_1_s : move = { key_pressed = Left; press_time = 1.0 }
 
-(*=
-X| |
------
- | |
------
- | |
-*)
-let state_after_move_at_0x0 : game_state =
-  { board = [ move_at_0x0, X ]
-  ; rows = 3
-  ; columns = 3
-  ; winning_sequence_length = 3
-  ; decision = In_progress { whose_turn = O }
+let state_after_move_at_1_s : game_state = 
+  { upcoming_notes = 
+       [ { timestamp = 1.5; direction = Up}
+       ; { timestamp = 2.0; direction = Down}]
+  ; life_bar = 100.0
+  ; score = 100
+  ; combo = 1
+  ; song_elapsed = 1.0 
+  ; decision = In_progress
   }
 ;;
 
-(*=
- | |X
------
-O|O|X
------
- | |
-*)
-let before_terminal_state : game_state =
-  { board =
-      [ { row = 0; column = 2 }, X
-      ; { row = 1; column = 0 }, O
-      ; { row = 1; column = 2 }, X
-      ; { row = 1; column = 1 }, O
-      ]
-  ; rows = 3
-  ; columns = 3
-  ; winning_sequence_length = 3
-  ; decision = In_progress { whose_turn = X }
+let before_teminal_state : game_state = 
+  { upcoming_notes = 
+       [ { timestamp = 2.0; direction = Down}]
+  ; life_bar = 5.0
+  ; score = 1000
+  ; combo = 0
+  ; song_elapsed = 44.5 
+  ; decision = In_progress
   }
 ;;
 
-let move_to_terminal_state : move = { row = 2; column = 2 }
+let move_to_terminal_state : move = { key_pressed = Right; press_time = 46.0 } 
 
-(*=
- | |X
------
-O|O|X
------
- | |X
-*)
-let terminal_state : game_state =
-  { board =
-      [ { row = 0; column = 2 }, X
-      ; { row = 1; column = 0 }, O
-      ; { row = 1; column = 2 }, X
-      ; { row = 1; column = 1 }, O
-      ; { row = 2; column = 2 }, X
-      ]
-  ; rows = 3
-  ; columns = 3
-  ; winning_sequence_length = 3
-  ; decision = Winner X
+let terminal_state : game_state = 
+  { upcoming_notes = 
+       []
+  ; life_bar = 0.0
+  ; score = 1000
+  ; combo = 0
+  ; song_elapsed = 46.0
+  ; decision = Stage_failed
   }
 ;;
