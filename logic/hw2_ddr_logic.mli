ff6ac700fca9c86cfd46016be256dcff32d3a9b0
Catherine Su <155842645+catherinesu-png@users.noreply.github.com>
2026-09-24 18:01:37 +0000

    Updated hw2.mli

--- logic/hw2_ddr_logic.mli
+++ logic/hw2_ddr_logic.mli
@@ -5,11 +5,13 @@ type arrow_direction =
   | Down
   | Up
   | Right
+[@@deriving sexp, compare, equal]
 
 type note =
   { timestamp : float
   ; direction : arrow_direction
   }
+[@@deriving sexp, compare, equal]
 
 type judgement =
   | Marvelous
@@ -17,11 +19,13 @@ type judgement =
   | Great
   | Good
   | Miss
+[@@deriving sexp, compare, equal]
 
 type decision =
   | In_progress
   | Stage_cleared of { final_score : int }
   | Stage_failed
+[@@deriving sexp, compare, equal]
 
 type game_state =
   { upcoming_notes : note list
@@ -31,11 +35,14 @@ type game_state =
   ; song_elapsed : float
   ; decision : decision
   }
+[@@deriving sexp, compare, equal]
 
 type move =
   { key_pressed : arrow_direction
   ; press_time : float
   }
+[@@deriving sexp, compare, equal]
 
 val evaluate_judgement : float -> judgement
+val update_time : game_state -> float -> game_state
 val make_move : game_state -> move -> game_state
